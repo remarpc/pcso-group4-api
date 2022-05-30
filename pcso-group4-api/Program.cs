82f8a1e7caccd9478a36e3d5804cd3e9c6ac3988
Richard Verdera, Jr <rverdera@pcso.gov.ph>
2022-05-30 10:43:01 +0800

    Impletement CRUD operations

--- pcso-group4-api/Program.cs
+++ pcso-group4-api/Program.cs
@@ -1,43 +1,93 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.SignalR;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<GameDb>(opt => opt.UseInMemoryDatabase("GameList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+builder.Services.AddSignalR();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/gameitems", async (GameDb db) =>
+    await db.Games.ToListAsync());
+
+app.MapPost("/gameitems", async (Game game, GameDb db) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    db.Games.Add(game);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/gameitems/{game.GameID}", game);
+});
+
+app.MapPut("/gameitems/{id}", async (int id, Game inputGame, GameDb db) =>
+{
+    var game = await db.Games.FindAsync(id);
+
+    if (game is null) return Results.NotFound();
+
+    game.GameID = inputGame.GameID;
+    game.Digit1 = inputGame.Digit1;
+    game.Digit2 = inputGame.Digit2;
+    game.Digit3 = inputGame.Digit3;
+    game.Digit4 = inputGame.Digit4;
+    game.Digit5 = inputGame.Digit5;
+    game.Digit6 = inputGame.Digit6;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/gameitems/{id}", async (int id, GameDb db) =>
+{
+    if (await db.Games.FindAsync(id) is Game game)
+    {
+        db.Games.Remove(game);
+        await db.SaveChangesAsync();
+        return Results.Ok(game);
+    }
+    return Results.NotFound();
+});
+
+app.MapDelete("/gameitems", async (GameDb db) =>
+{
+    await db.Database.EnsureDeletedAsync();
+    await db.SaveChangesAsync();
+    return Results.Ok(null);
+});
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+class Game
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int Id { get; set; }
+    public int GameID { get; set; }
+    public int Digit1 { get; set; }
+    public int Digit2 { get; set; }
+    public int Digit3 { get; set; }
+    public int Digit4 { get; set; }
+    public int Digit5 { get; set; }
+    public int Digit6 { get; set; }
 }
+
+class GameDb : DbContext
+{
+    public GameDb(DbContextOptions<GameDb> options)
+        : base(options) { }
+
+    public DbSet<Game> Games => Set<Game>();
+}
+
